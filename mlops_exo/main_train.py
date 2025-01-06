3bcb58f2d7124629ff2017b6bc9c56ca8893c71a
Thibaud Vienne <vienne.thibaud@gmail.com>
2025-01-06 14:35:24 +0100

    [ADD] reset exercice 3.3

--- mlops_exo/main_train.py
+++ mlops_exo/main_train.py
@@ -5,7 +5,6 @@ from mlops_exo.gathering.cleaning import DataCleaner
 from mlops_exo.features.task import FeaturesEngineering
 from mlops_exo.ml.task import train_model, predict_with_model
 from mlops_exo.ml.validation import split_train_and_val_sets, compute_metrics
-from mlops_exo.ml.utils import print_mlflow_artefact_uri
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -70,50 +69,33 @@ def main():
     joblib.dump(model, "../models/model.pkl")
 
     # save model
-    import mlflow
-    from mlflow.models import infer_signature
     # TODO - exercice 3.3 : lancer le run MLFlow et assignez un nom à l'exérimentation
     # ------------------------------------------------------------------------------------
-    mlflow.set_experiment("Exo MLOps - Random Forest")
-    with mlflow.start_run():
-        artifact_uri = mlflow.get_artifact_uri()
-        print(f"Les artefacts sont sauvegardés ici : {artifact_uri}")
+    #
     # ------------------------------------------------------------------------------------
 
-        # TODO - exercice 3.3 : enregistrer les paramètres et se trouvant dans dict_params
-        # ------------------------------------------------------------------------------------
-        mlflow.log_param("max_depth", dict_params["max_depth"])
-        mlflow.log_param("n_estimators", dict_params["n_estimators"])
-        mlflow.log_param("min_samples_split", dict_params["min_samples_split"])
-        mlflow.log_param("random_state", dict_params["random_state"])
-        # ------------------------------------------------------------------------------------
+    # TODO - exercice 3.3 : enregistrer les paramètres et se trouvant dans dict_params
+    # ------------------------------------------------------------------------------------
+    #
+    # ------------------------------------------------------------------------------------
 
-        # TODO - exercice 3.3 : enregistrer les métriques dans dict_metrics_train et dict_metrics_val
-        # ------------------------------------------------------------------------------------
-        mlflow.log_metric("mae_train", dict_metrics_train["mae_train"])
-        mlflow.log_metric("mse_train", dict_metrics_train["mse_train"])
-        mlflow.log_metric("mape_train", dict_metrics_train["mape_train"])
-        mlflow.log_metric("mae_val", dict_metrics_val["mae_val"])
-        mlflow.log_metric("mse_val", dict_metrics_val["mse_val"])
-        mlflow.log_metric("mape_val", dict_metrics_val["mape_val"])
-        # ------------------------------------------------------------------------------------
+    # TODO - exercice 3.3 : enregistrer les métriques dans dict_metrics_train et dict_metrics_val
+    # ------------------------------------------------------------------------------------
+    #
+    # ------------------------------------------------------------------------------------
 
-        # TODO - exercice 3.3 : enregistrer les artefacts
-        # ------------------------------------------------------------------------------------
-        mlflow.log_artifact("../models/cleaner.pkl")
-        mlflow.log_artifact("../models/features_transformer.pkl")
-        mlflow.log_artifact("../data/raw/features.csv")
-        mlflow.log_artifact("../data/raw/stores.csv")
-        # ------------------------------------------------------------------------------------
+    # TODO - exercice 3.3 : enregistrer les artefacts
+    # ------------------------------------------------------------------------------------
+    #
+    # ------------------------------------------------------------------------------------
 
-        # TODO - exercice 3.3 : enregistrer le modèle et la signature
-        # ------------------------------------------------------------------------------------
-        signature = infer_signature(x_train, model.predict(x_train))
-        mlflow.sklearn.log_model(model, "model", signature=signature, input_example=x_train.iloc[0:1])
-        # ------------------------------------------------------------------------------------
+    # TODO - exercice 3.3 : enregistrer le modèle et la signature
+    # ------------------------------------------------------------------------------------
+    #
+    # ------------------------------------------------------------------------------------
 
     # affichage du lieu de sauvegarde des artefacts
-    print_mlflow_artefact_uri()
+    # print_mlflow_artefact_uri() # TODO - exercice 3.3 : à ré-activer
 
 
 if __name__ == "__main__":
