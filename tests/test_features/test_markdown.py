31fcff200285ac0e0d45852b7635603e3c55c8f6
Margot Fournier <margot.fournier@hymaia.com>
2025-08-05 16:34:54 +0200

    feat: test_add_total_markdowns_all_values

--- tests/test_features/test_markdown.py
+++ tests/test_features/test_markdown.py
@@ -7,14 +7,25 @@ from mlops_exo.features.markdown import add_total_markdown
 
 def test_add_total_markdowns_all_values():
     # Given : input markdowns
-    #
+    df = pd.DataFrame(data={
+        'MarkDown1': [10],
+        'MarkDown2': [20], 
+        'MarkDown3': [30],
+        'MarkDown4': [40],
+        'MarkDown5': [50]
+    })
+    expected_output = pd.DataFrame(data={'MarkDown1': [10],
+                                          'MarkDown2': [20], 
+                                          'MarkDown3': [30], 
+                                          'MarkDown4': [40], 
+                                          'MarkDown5': [50], 
+                                          'MarkdownsSum': [150]})
 
     # When
-    #
+    output = add_total_markdown(df)
 
     # Then
-    #
-    pass
+    pd.testing.assert_frame_equal(output, expected_output)
 
 
 # TODO 3.2 : tester la fonction add_total_markdown avec seulement 3 colonnes markdown dispos
