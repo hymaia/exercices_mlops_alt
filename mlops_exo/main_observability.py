c9999cd1076de3bd74c3eeef787145749c14e711
Margot Fournier <margot.fournier@hymaia.com>
2025-08-08 19:06:30 +0200

    feat: solution 4.3.B

--- mlops_exo/main_observability.py
+++ mlops_exo/main_observability.py
@@ -7,14 +7,18 @@ from config import DATA_PROCESSED, REPORTS_DIR
 if __name__ == "__main__":
     # Exercice 4.3.A : créer les jeux de référence et current
     # ------------------------------------------------------------------------------------
-    x_train_processed = pd.read_parquet(DATA_PROCESSED / "x_train_processed.parquet").set_index("Date")
-    x_val_processed = pd.read_parquet(DATA_PROCESSED / "x_val_processed.parquet").set_index("Date")
+    x_train_processed = pd.read_parquet(
+        DATA_PROCESSED / "x_train_processed.parquet"
+    ).set_index("Date")
+    x_val_processed = pd.read_parquet(
+        DATA_PROCESSED / "x_val_processed.parquet"
+    ).set_index("Date")
     y_train = pd.read_parquet(DATA_PROCESSED / "y_train.parquet")
     y_val = pd.read_parquet(DATA_PROCESSED / "y_val.parquet")
     pred_train = pd.read_parquet(DATA_PROCESSED / "pred_train.parquet")
     pred_val = pd.read_parquet(DATA_PROCESSED / "pred_val.parquet")
 
-    y_train.index= x_train_processed.index
+    y_train.index = x_train_processed.index
     y_val.index = x_val_processed.index
 
     pred_train.index = x_train_processed.index
@@ -24,23 +28,56 @@ if __name__ == "__main__":
     df_current = pd.concat([x_val_processed, y_val, pred_val], axis=1)
     # ------------------------------------------------------------------------------------
 
-    # TODO - exercice 4.3.B : réaliser le ColumnMapping
+    # Exercice 4.3.B : réaliser le ColumnMapping
     # ------------------------------------------------------------------------------------
-    # target =
-    # prediction =
-    #  numerical_features = []
-    # categorical_features = []
-    # data_def = DataDefinition(
-    #     numerical_columns=numerical_features,
-    #     categorical_columns=categorical_features,
-    #     regression=[Regression(target=target, prediction=prediction)],
-    # )
-    # dataset_reference = 
-    # dataset_current =
-    # dataset_current_sample = 
-    #
-    # ------------------------------------------------------------------------------------
+    target = "Weekly_Sales"
+    prediction = "prediction"
+    date_col = "Date"
+    numerical_features = [
+        "Temperature",
+        "Fuel_Price",
+        "MarkDown1",
+        "MarkDown2",
+        "MarkDown3",
+        "MarkDown4",
+        "MarkDown5",
+        "CPI",
+        "Unemployment",
+        "Size",
+        "MarkdownsSum",
+        "Days_to_Thansksgiving",
+        "Days_to_Christmas",
+    ]
 
+    categorical_features = [
+        "Store",
+        "Dept",
+        "IsHoliday",
+        "Type",
+        "Day",
+        "Week",
+        "Month",
+        "Year",
+        "SuperBowlWeek",
+        "LaborDayWeek",
+        "ThanksgivingWeek",
+        "ChristmasWeek",
+    ]
+
+    data_def = DataDefinition(
+        numerical_columns=numerical_features,
+        categorical_columns=categorical_features,
+        regression=[Regression(target=target, prediction=prediction)],
+    )
+
+    # Convert pandas DataFrames to Evidently Dataset objects
+    dataset_reference = Dataset.from_pandas(df_reference, data_definition=data_def)
+    dataset_current = Dataset.from_pandas(df_current, data_definition=data_def)
+    dataset_current_sample = Dataset.from_pandas(
+        df_current.sample(10_000, random_state=42), data_definition=data_def
+    )
+
+    # ------------------------------------------------------------------------------------
     # créer un rapport de Model Drift
     model_drift_report = Report(
         metrics=[
