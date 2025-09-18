aaf8fb81829d5953c74d8fe411c8f336988d8694
Anis ZAKARI <73587508+AnisZakari@users.noreply.github.com>
2025-09-18 05:07:33 +0000

    fix port issue

--- mlops_exo/main_live_inference.py
+++ mlops_exo/main_live_inference.py
@@ -6,8 +6,10 @@ import sys
 import warnings
 import mlflow
 import time
-warnings.filterwarnings('ignore')
 
+warnings.filterwarnings("ignore")
+
+PORT=5000
 
 ##########################################################
 # PART 1 : inference with all features already computes  #
@@ -15,36 +17,37 @@ warnings.filterwarnings('ignore')
 
 print("----- PARTIE 1 : prédiction avec toutes les features déjà données")
 data_json_1 = {
-    "dataframe_records": [{"Store": 8986.8302954228,
-                           "Dept": 11883.1919251024,
-                           "Temperature": 30.34,
-                           "Fuel_Price": 3.811,
-                           "MarkDown1": 0.0,
-                           "MarkDown2": 0.0,
-                           "MarkDown3": 0.0,
-                           "MarkDown4": 0.0,
-                           "MarkDown5": 0.0,
-                           "CPI": 134.0682581,
-                           "Unemployment": 7.658,
-                           "Size": 123737.0,
-                           "MarkdownsSum": 0.0,
-                           "Day": 1.0,
-                           "Week": 13.0,
-                           "Month": 4.0,
-                           "Year": 2011.0,
-                           "Days_to_Thansksgiving": 237.0,
-                           "Days_to_Christmas": 267.0,
-                           "SuperBowlWeek": 0.0,
-                           "LaborDayWeek": 0.0,
-                           "ThanksgivingWeek": 0.0,
-                           "ChristmasWeek": 0.0
-                           }
-                          ]
-
+    "dataframe_records": [
+        {
+            "Store": 8986.8302954228,
+            "Dept": 11883.1919251024,
+            "Temperature": 30.34,
+            "Fuel_Price": 3.811,
+            "MarkDown1": 0.0,
+            "MarkDown2": 0.0,
+            "MarkDown3": 0.0,
+            "MarkDown4": 0.0,
+            "MarkDown5": 0.0,
+            "CPI": 134.0682581,
+            "Unemployment": 7.658,
+            "Size": 123737.0,
+            "MarkdownsSum": 0.0,
+            "Day": 1.0,
+            "Week": 13.0,
+            "Month": 4.0,
+            "Year": 2011.0,
+            "Days_to_Thansksgiving": 237.0,
+            "Days_to_Christmas": 267.0,
+            "SuperBowlWeek": 0.0,
+            "LaborDayWeek": 0.0,
+            "ThanksgivingWeek": 0.0,
+            "ChristmasWeek": 0.0,
+        }
+    ]
 }
 
 # Envoyez une requête POST à l'URL du modèle
-response = requests.post("http://0.0.0.0:5050/invocations", json=data_json_1)
+response = requests.post(f"http://0.0.0.0:{PORT}/invocations", json=data_json_1)
 
 # Affichez la prédiction
 
@@ -58,12 +61,14 @@ sys.exit(0)  # TODO 4.1.D : supprimez cette ligne
 
 print("\n----- PARTIE 2 : prédiction avec un script d'inférence")
 data_sales_2 = {
-    "dataframe_records": [{
-                        "Store": 10,
-                        "Dept": 5,
-                        "Date": "2011-04-01",
-                        "IsHoliday": False,
-                        }]
+    "dataframe_records": [
+        {
+            "Store": 10,
+            "Dept": 5,
+            "Date": "2011-04-01",
+            "IsHoliday": False,
+        }
+    ]
 }
 df_sales_2 = pd.DataFrame(data_sales_2["dataframe_records"])
 
@@ -96,10 +101,9 @@ artifact_uri = f"mlruns/0/{mlflow_run_id}/artifacts"
 # ------------------------------------------------------------------------------------
 
 
-
 # Envoyez une requête POST à l'URL du modèle
 data_json_2 = {"dataframe_records": df_sales_2.to_dict(orient="records")}
-response = requests.post("http://0.0.0.0:5050/invocations", json=data_json_2)
+response = requests.post(f"http://0.0.0.0:{PORT}/invocations", json=data_json_2)
 
 # Affichez la prédiction
 print("Prédiction obtenue : %s " % response.json())
@@ -120,7 +124,9 @@ def predict_with_monitoring(data_json_to_predict: dict):
     # prepare data and log latency
     start_time = time.time()
     try:
-        prediction = requests.post("http://0.0.0.0:5050/invocations", json=data_json_to_predict).json()["predictions"][0]
+        prediction = requests.post(
+            f"http://0.0.0.0:{PORT}/invocations", json=data_json_to_predict
+        ).json()["predictions"][0]
         is_error = 0
     except:
         prediction = 0
@@ -146,7 +152,6 @@ def log_response_info(latency: float, is_error: float, is_very_high_value: float
 # Définissez cette expérimentation comme active
 mlflow.set_experiment("4.2 monitoring")
 with mlflow.start_run() as run:
-
     # on charge le jeu de test
     print("\n----- PARTIE 3 : monitoring and alerting")
     df_test = pd.read_csv("../data/raw/test.csv")
@@ -155,7 +160,11 @@ with mlflow.start_run() as run:
     for i in range(100):
         time.sleep(0.1)
         current_instance = pd.DataFrame(df_test.iloc[i]).T
-        df_processed = prepare_and_transform_data(current_instance, df_features, df_stores)
-        data_json_to_predict = {"dataframe_records": df_processed.to_dict(orient="records")}
+        df_processed = prepare_and_transform_data(
+            current_instance, df_features, df_stores
+        )
+        data_json_to_predict = {
+            "dataframe_records": df_processed.to_dict(orient="records")
+        }
         prediction = predict_with_monitoring(data_json_to_predict)
         print("live inference (%s) : prediction = %s" % (i, prediction))
